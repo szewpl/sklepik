5ccb974cfe8ca4ab918032a44f4bddfc0c70d16a
szewpl <84736858+szewpl@users.noreply.github.com>
2022-05-22 19:31:46 +0200

    es

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
     require("szablon.html");
     $mysqli = new mysqli("localhost", "root", "", "sklepik");
-    $zxc = "SELECT * FROM sdzieciecy";
+    $zxc = "SELECT * FROM produkty";
     $all = $mysqli->query($zxc);
     foreach($all as $one)
     {
         echo($one["name"]);
     }
-    echo "witam";
 ?>  
