944f33baa21f47ba418969276150eead5330d93d
szewpl <84736858+szewpl@users.noreply.github.com>
2022-05-24 11:56:51 +0200

    es

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ $(function(){
     });
     
     // specify the server/url you want to load data from
-    var url = "http://localhost/sklepik/index.php";
+    var url = "http://localhost/sklepik/konto.php";
     
     // on click, load the data dynamically into the #result div
     $("#b1").click(function(){
         $("body").load(url);
-        window.history.pushState("object or string", "Title", "/witam");
+        window.history.pushState("object or string", "Title", "http://localhost/sklepik/#konto");
     });
-
+    window.onhashchange = function(){
+        url="http://localhost/sklepik/index.php";
+        $("body").load(url);
+    };
 });
