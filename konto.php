dc0f30ed10261d558c1d42d09c5d373acaf6c53e
szewpl <szewgiga@gmail.com>
2022-05-24 20:31:37 +0200

    es

--- konto.php
+++ konto.php
@@ -1,10 +1,4 @@
 <?php
     require("szablon.html");
-    $mysqli = new mysqli("localhost", "root", "", "sklepik");
-    $zxc = "SELECT * FROM produkty";
-    $all = $mysqli->query($zxc);
-    foreach($all as $one)
-    {
-        echo($one["id"]);
-    }
+    require("konto.html");
 ?>  
--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-$(function(){
-    // optional: don't cache ajax to force the content to be fresh
-    $.ajaxSetup ({
-        cache: true,
-    });
-    
-    // specify the server/url you want to load data from
-    var url = "http://localhost/sklepik/konto.php";
-    
-    // on click, load the data dynamically into the #result div
-    $("#b1").click(function(){
-        $("body").load(url);
-        window.history.pushState("object or string", "Title", "http://localhost/sklepik/#konto");
-    });
-    window.onhashchange = function(){
-        url="http://localhost/sklepik/index.php";
-        $("body").load(url);
-    };
-});
+const b = document.getElementById("textkonto");
+if(!localStorage.getItem("konto")){
+   b.innerText = "Zaloguj się";
+}else{
+    b.innerText = "Konto, " + localStorage.getItem("konto");
+}
