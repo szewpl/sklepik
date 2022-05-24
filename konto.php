5f5ed0f0e2526d2d1a0080f95829b6efa2c4983c
szewpl <84736858+szewpl@users.noreply.github.com>
2022-05-25 00:59:32 +0200

    es

--- konto.php
+++ konto.php
@@ -1,4 +1,21 @@
 <?php
     require("szablon.html");
     require("konto.html");
+    $conn = new mysqli("localhost","root","","sklepik");
+    $user = $_POST['username'];
+    $mail = $_POST['email'];
+    $password = $_POST['password'];
+    $query = "SELECT * FROM konto WHERE user='$user'";
+    $result=$conn->query($query);
+    if($result->num_rows>0){
+        echo "<div>Taka nazwa użytkownika juz istnieje</div><script>rejestracja();</script>";
+    }else{
+        $query1 = "INSERT INTO konto(user,mail,password) VALUES ('$user','$mail','$password')";
+        $conn->query($query1);
+        echo "<script>
+        localStorage.setItem('konto','$user');
+        location.href='http://localhost/sklepik/';
+        </script>";
+    }
+    $conn->close();
 ?>  
